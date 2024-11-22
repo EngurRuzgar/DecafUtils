7e113121216ad3a685c7a3ea813e479d306bbaad
EngurRuzgar <info@nucceteere.xyz>
2024-11-23 00:22:22 +0300

    init: add base project

---
+++ src/main/java/xyz/nucceteere/decafutils/DecafUtils.java
@@ -0,0 +1,32 @@
+package xyz.nucceteere.decafutils;
+
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.Logger;
+
+@Mod(modid = DecafUtils.MODID, name = DecafUtils.NAME, version = DecafUtils.VERSION)
+
+public class DecafUtils
+{
+    public static final String MODID = "decafutils";
+    public static final String NAME = "DecafSinatra Utilities";
+    public static final String VERSION = "0.1";
+
+    private static Logger logger;
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+        logger = event.getModLog();
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+        // some example code
+        logger.info("DecafUtils by Nucceteere loaded");
+    }
+}
